1763879c4a5619fcc44725cea46226db6f7a8977
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-04-26 20:56:32 +0530

    added assets, avatars

---
+++ src/assets/data.js
@@ -0,0 +1,15 @@
+const avatars = [
+  {
+    id: 1,
+    type: "male",
+    info: "Avatar name 'Avatown' -nice original avatar of Avatown",
+    rating: 5,
+    likes: 100,
+    price: "$50.00",
+    fits: "PC only",
+    additionalInfo:
+      "Auto upload service ready, you can use this avatar within 24 hours",
+  },
+];
+
+export { avatars };
