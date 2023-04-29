e9811b03740fb1263e71f736064acdc6738a7035
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-04-29 18:17:46 +0530

    added specific js file

--- src/page/avatar/Specific.jsx
+++ src/page/avatar/Specific.jsx
@@ -1,8 +1,17 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Specific() {
   const { id } = useParams();
-  return <div>This page {id}</div>;
+  return (
+    <div className="p-5">
+      <Link to="/" className="btn ">
+        Back
+      </Link>
+
+      <h3> This page {id}</h3>
+    </div>
+  );
 }
 
 export default Specific;
