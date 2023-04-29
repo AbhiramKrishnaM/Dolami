811f628a82b34bc73b5309925ddacc8193c149d3
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-04-29 15:08:16 +0530

    added mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Avatar from "./components/AvatarCards";
 function App() {
   const cards = avatars.map((avatar) => <Avatar key={avatar.id} {...avatar} />);
 
-  return <div className="grid grid-cols-3 gap-3">{cards}</div>;
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-screen">
+      {cards}
+    </div>
+  );
 }
 
 export default App;
--- src/components/AvatarCards.jsx
+++ src/components/AvatarCards.jsx
@@ -43,7 +43,7 @@ function Avatar(props) {
         </div>
 
         <div className="mt-3">
-          <h3 className="text-3xl ">{props.info}</h3>
+          <h3 className="text-2xl lg:text-3xl ">{props.info}</h3>
         </div>
 
         <div className="flex items-center space-x-4 mt-3">
@@ -70,16 +70,21 @@ function Avatar(props) {
               <title>Rating star</title>
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
-            <p className="ml-2 text-sm font-bold text-gray-900 ">4.95</p>
+            <p className="ml-2 text-sm font-bold text-gray-900 ">
+              {props.rating}
+            </p>
             <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full "></span>
-            <a href="#" className="text-sm font-medium text-gray-900">
+            <a
+              href="#"
+              className="text-xs md:text-sm font-medium text-gray-900"
+            >
               {props.likes} likes
             </a>
           </div>
 
           <div className="text-end flex items-end justify-end">
-            <span className="text-lg mr-1">$</span>
-            <h4 className="text-3xl font-medium">{props.price}</h4>
+            <span className="text-base md:text-lg mr-1">$</span>
+            <h4 className="text-2xl md:text-3xl font-medium">{props.price}</h4>
           </div>
         </div>
 
@@ -87,19 +92,19 @@ function Avatar(props) {
           <Icon icon="material-symbols:desktop-windows-rounded" width={25} />
           <span>{props.fits}</span>
         </div>
-        <div className="mt-3  flex items-center ">
-          <span>{props.additionalInfo}</span>
+        <div className="mt-3  flex items-center gap-4">
+          <span className="text-sm lg:text-base">{props.additionalInfo}</span>
           {!isCopied ? (
             <Tooltip content="Copy Url" style="dark" arrow={false}>
               <Icon
                 icon="material-symbols:content-copy-outline"
-                width={30}
+                width={20}
                 onClick={copied}
               />
             </Tooltip>
           ) : (
             <Tooltip content="Copied Url" style="dark" arrow={false}>
-              <Icon icon="icon-park-twotone:success" width={30} />
+              <Icon icon="icon-park-twotone:success" width={20} />
             </Tooltip>
           )}
         </div>
