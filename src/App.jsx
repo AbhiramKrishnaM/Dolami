cd18c6e417979e90288581bf0bbe530fcdb9388b
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-04-29 15:29:06 +0530

    added log and added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { avatars } from "../public/assets/data";
 import Avatar from "./components/AvatarCards";
+import Navbar from "./components/Navbar";
 
 function App() {
   const cards = avatars.map((avatar) => <Avatar key={avatar.id} {...avatar} />);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-screen">
-      {cards}
+    <div className="w-screen">
+      <Navbar />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-full mt-3">
+        {cards}
+      </div>
     </div>
   );
 }
