efb196bb84727000ba6282c10a9749b8b1acf269
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-04-29 16:24:08 +0530

    added updates to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { avatars } from "../public/assets/data";
 import Avatar from "./components/AvatarCards";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Sidenavbar from "./components/Sidenavbar";
 
 function App() {
   const cards = avatars.map((avatar) => <Avatar key={avatar.id} {...avatar} />);
@@ -9,8 +10,12 @@ function App() {
   return (
     <div className="w-screen">
       <Navbar />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-full mt-3">
-        {cards}
+
+      <div className="flex">
+        <Sidenavbar />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-full mt-3">
+          {cards}
+        </div>
       </div>
 
       <Footer />
---
+++ src/components/Sidenavbar.jsx
@@ -0,0 +1,5 @@
+function Sidenavbar() {
+  return <div className="min-h-screen w-[250px] p-4">Sidenavbar</div>;
+}
+
+export default Sidenavbar;
