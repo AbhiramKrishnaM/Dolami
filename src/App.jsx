60dcacd2e529d897e5f69b8b256bff1c8a9213ed
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-04-29 12:12:52 +0530

    replaced files and assets and renamed components and created cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { avatars } from "../public/assets/data";
-import AvatarCard from "./components/AvatarCards";
+import { avatars } from "./assets/data";
+import Avatar from "./components/AvatarCards";
 
 function App() {
-  const Cards = avatars.map((avatar) => (
-    <AvatarCard key={avatar.id} {...avatar} />
-  ));
-  return Cards;
+  const cards = avatars.map((avatar) => <Avatar key={avatar.id} {...avatar} />);
+
+  return <div className="grid grid-cols-3 gap-3">{cards}</div>;
 }
 
 export default App;
--- src/components/AvatarCards.jsx
+++ src/components/AvatarCards.jsx
@@ -1,16 +1,17 @@
-/* eslint-disable react/prop-types */
 function Avatar(props) {
   return (
     <>
-      <div>{props.type}</div>
-      <div>{props.info}</div>
-      <div>{props.url}</div>
-      <div>{props.alt}</div>
-      <div>{props.rating}</div>
-      <div>{props.likes}</div>
-      <div>{props.price}</div>
-      <div>{props.fits}</div>
-      <div>{props.additionalInfo}</div>
+      <div className="p-4 m-2 rounded-xl bg-white shadow-xl">
+        <div>{props.type}</div>
+        <div>{props.info}</div>
+        <div>{props.url}</div>
+        <div>{props.alt}</div>
+        <div>{props.rating}</div>
+        <div>{props.likes}</div>
+        <div>{props.price}</div>
+        <div>{props.fits}</div>
+        <div>{props.additionalInfo}</div>
+      </div>
     </>
   );
 }
